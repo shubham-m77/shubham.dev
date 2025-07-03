fb6f7d742442c102dbf872be90e6f2ea3fb9c9ad
Shubham Mandal <shubham.m9022@gmail.com>
2025-07-03 13:25:47 +0530

    updated footer

--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -21,8 +21,26 @@ const footerLinks= [
   },
   {
     title:"Resume",
-    icon: <svg width="24" height="24" xmlns="http://www.w3.org/2000/svg" fill-rule="evenodd" clip-rule="evenodd"><path d="M22 0h-20v24h14l6-6v-18zm-7 23h-12v-22h18v16h-6v6zm1-5h4.586l-4.586 4.586v-4.586zm-3 1h-8v1h8v-1zm0-3h-8v1h8v-1zm6-2v-1h-14v1h14zm0-4h-4v1h4v-1zm-6.006 1h-7.991l-.003-.789c-.003-.72-.006-1.615 1.314-1.92 1.483-.341 1.236-.418 1.158-.563-1.078-1.988-.71-3.173-.395-3.703.388-.651 1.089-1.025 1.923-1.025.827 0 1.523.368 1.91 1.011.545.904.409 2.222-.379 3.713-.105.196-.195.255 1.119.559 1.355.312 1.352 1.212 1.35 1.936l-.006.781zm-6.994-1h6c-.007-.547-.07-.626-.54-.734-.855-.198-1.629-.376-1.901-.972-.142-.311-.113-.66.087-1.039.61-1.151.758-2.146.407-2.729-.276-.458-.778-.526-1.053-.526-.48 0-.857.19-1.063.537-.352.59-.201 1.58.414 2.714.204.377.236.727.095 1.039-.269.598-1.036.774-1.847.962-.525.121-.593.202-.599.748zm13-2v-1h-4v1h4zm0-4h-4v1h4v-1z"/></svg>,
-    href:"https://drive.google.com/file/d/1SJ3WYsrQESULAfCr2XZZ_kVdhHPS54Aw/view?usp=sharing",
+    icon:
+  <svg
+    width="24"
+    height="24"
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    stroke="#fff"
+    strokeWidth="2"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+    viewBox="0 0 24 24"
+  >
+    <path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z" />
+    <polyline points="14 2 14 8 20 8" />
+    <line x1="16" y1="13" x2="8" y2="13" />
+    <line x1="16" y1="17" x2="8" y2="17" />
+    <polyline points="10 9 9 9 8 9" />
+  </svg>,
+  
+  href:"https://drive.google.com/file/d/1SJ3WYsrQESULAfCr2XZZ_kVdhHPS54Aw/view?usp=sharing",
   },
 
 ]
@@ -32,16 +50,17 @@ export const Footer = () => {
     <div className="px-4 md:px-14 lg:px-20">
       <div className="border-t justify-center md:justify-between gap-8 md:gap-0  flex flex-col-reverse  md:flex-row items-center border-white/15 py-6 text-sm">
         <div className="text-white/80">&copy; 2025 All Rights Reserved</div>
-        <nav className="flex justify-center flex-col md:flex-row gap-5">
+        <nav className="flex justify-center flex-wrap md:flex-row gap-6 z-10 relative">
           {footerLinks.map(link=>
 <a
   key={link.title}
   href={link.href}
   target="_blank"
+  title={link.title}
 
-  className="font-medium gap-1.5 inline-flex items-center text-white"
+  className="font-medium gap-1.5 inline-flex items-center z-10 relative text-white"
 >
-  <span className="size-6">{link.icon}</span>{link.title}
+  <span className="size-6">{link.icon}</span>
 </a>
           )}
         </nav>
