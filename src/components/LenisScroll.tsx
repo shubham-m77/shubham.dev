844c66624afb025f054280d3c05e0cea24f6a816
Shubham Mandal <shubham.m9022@gmail.com>
2025-07-22 14:53:31 +0530

    Fixed Bugs

--- src/components/LenisScroll.tsx
+++ src/components/LenisScroll.tsx
@@ -1,22 +1,14 @@
-<<<<<<< HEAD
+"use client";
 import { ReactLenis } from 'lenis/react';
 
 export function LenisScroll({ children }: { children: React.ReactNode }) {
-    // No need to use useLenis here unless you want to control scroll programmatically
-=======
-"use client";
-import { ReactLenis, useLenis } from 'lenis/react';
-export function LenisScroll({ children }: { children: any }) {
-    const lenis = useLenis((lenis) => {
->>>>>>> f35df072c52710dbca884999db1a73b4f9da3011
-
     return (
         <ReactLenis
             root
             options={{
-                lerp: 0.1, // lower = smoother, higher = snappier (default is 0.1)
-                duration: 1.2, // seconds, adjust for more/less smoothness
-                touchMultiplier: 1.5, // makes touch scroll feel more natural
+                lerp: 0.1, // lower = smoother, higher = snappier
+                duration: 1.2, // duration in seconds for scroll animations
+                touchMultiplier: 1.5, // enhances touch scroll feel
             }}
         >
             {children}
